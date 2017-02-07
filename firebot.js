9a7d16626a53ccb87d54e08498694030d97b8279
Haley <haleymthurston@gmail.com>
2017-02-07 15:41:57 -0800

    move constants

--- firebot.js
+++ firebot.js
@@ -1,12 +1,77 @@
-/* Main Firebot functionality */
+/* FIREBOT */
 
 if (!process.env.token) {
   console.log('Error: Specify token in environment');
   process.exit(1);
 }
 
+/*
+  CONSTANTS
+*/
+
+/*
+  List of message subtypes that count as 'real messages' for measuring activity.
+  Default messages do not have a subtype.
+*/
+
+var subtypeWhitelist = [
+  'file_comment',
+  'file_mention',
+  'file_share',
+  'message_replied',
+  'reply_broadcast',
+  'bot_message'
+];
+
+/* Default firebot responses by type */
+
+var negative = ['no', 'no.', 'nope', 'not at all', 'no, only hales', 'uh..', 'lol', 'sorry man', 'meh', 'nah', 'ha', 'never', 'definitely not'];
+var positive = ['very much so', 'absolutely', 'def', 'ya'];
+var indifferent =['fine', 'meh', 'sure', 'hm', 'no'];
+var grabBag = negative.concat(positive).concat(indifferent);
+var responses = { negative, positive, indifferent, grabBag };
+
+
+/*
+  Defines the settings for each of the different types of channel history API calls.
+  Milliseconds in a day: 86400000
+  Milliseconds in 15 minutes: 900000
+*/
+
+var historyConfig = {
+  'daily': {
+    timeOffset: 86400000,
+    messageMinimum: 19,
+    channelList: 'dailyActiveChannels',
+    emptyListText: 'No channels have been busy lately',
+  },
+  'recent': {
+    timeOffset: 900000,
+    messageMinimum: 9,
+    channelList: 'recentActiveChannels'
+  },
+  'dead': {
+    timeOffset: 86400000 * 7,
+    messageMinimum: null,
+    channelList: 'deadChannels',
+    emptyListText: 'No dead channels right now',
+  },
+};
+
+/*
+  Defines the default interval for how often firebot should detect activity
+  Milliseconds in 10 minutes: 600000
+*/
+
+var defaultInterval = 600000;
+
+
+
+/*
+  MAIN FIREBOT FUNCTIONALITY
+*/
+
 var Botkit = require('botkit');
-var { subtypeWhitelist, responses, defaultInterval, historyConfig } = require('./constants');
 
 var Firebot = {
   allUsers: [],
@@ -59,7 +124,7 @@ var Firebot = {
     }.bind(this));
 
     controller.on('channel_archive', function(bot,res) {
-      for (var c in bot.allChannels) {
+      for (var c in this.allChannels) {
         if (this.allChannels[c].id === res.channel) {
           this.allChannels.splice(c, 1);
         }
